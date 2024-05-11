0dc98da801cd3e642c028ccd3ea2c1dd889a5e86
AbdallahSlimane <slimane.abdallah75@gmail.com>
2024-05-11 15:01:22 +0200

    add execution code

--- dockerfiles/java/App-1.java
+++ dockerfiles/java/App-1.java
@@ -1,9 +1,1 @@
-/**
- * Hello world!
- *
- */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World Toto!");
-    }
-}
+public class HelloWorld { public static void main(String[] args) { System.out.println("Hello, World!"); } }
