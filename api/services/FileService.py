d715fa6e65281c3235d4a6c1579e9fbb9ffd5bb9
melyLaur <melissanoemi.laurent@free.fr>
2024-06-18 22:47:47 +0200

    clean code

--- api/services/FileService.py
+++ api/services/FileService.py
@@ -1,21 +1,21 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class FileService:
     def __init__(self):
-        self.base_folder_code_resource = "./coderesources"
         self.default_filename_code = "App."
 
     def createFolderCodeResources(self, codeResources):
-        folder_path = self.base_folder_code_resource + "/" + codeResources.uuid
+        base_folder_code_resource = os.getenv('STORAGE_PATH')
+        folder_path = base_folder_code_resource + "/" + codeResources.uuid
 
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
         self.createCodeFile(codeResources.code, folder_path, codeResources.language)
-
-        # todo : create assets file
-
         return folder_path
 
     def createCodeFile(self, code, folder_path, language):
