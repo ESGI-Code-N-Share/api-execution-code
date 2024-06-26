7be97409d0831cd6daf4ff09c2c02ae55cafebb3
melyLaur <melissanoemi.laurent@free.fr>
2024-06-26 23:13:27 +0200

    WIP : implement S3

--- api/services/FileService.py
+++ api/services/FileService.py
@@ -1,7 +1,10 @@
 import os
 from dotenv import load_dotenv
+from config import create_s3_client
+
 
 load_dotenv()
+s3_client = create_s3_client()
 
 
 class FileService:
@@ -9,13 +12,16 @@ class FileService:
         self.default_filename_code = "App."
 
     def createFolderCodeResources(self, codeResources):
-        base_folder_code_resource = os.getenv('STORAGE_PATH')
+        base_folder_code_resource = os.getenv('LOCAL_STORAGE_PATH')
         folder_path = base_folder_code_resource + "/" + codeResources.uuid
 
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
         self.createCodeFile(codeResources.code, folder_path, codeResources.language)
+
+        # todo : créer une condition code ressources
+        self.downloadResources(codeResources.uuid)
         return folder_path
 
     def createCodeFile(self, code, folder_path, language):
@@ -23,3 +29,47 @@ class FileService:
         file_path_code = folder_path + "/" + self.default_filename_code + extension
         with open(file_path_code, 'w') as file:
             file.write(code)
+
+    def downloadResources(self, uuid):
+        # todo : clean le code
+        AWS_S3_BUCKET_NAME = os.getenv('AWS_S3_BUCKET_NAME')
+        dossier_local = os.getenv('LOCAL_STORAGE_PATH') + uuid + '/'
+
+        if not os.path.exists(dossier_local):
+            os.makedirs(dossier_local)
+
+        continuation_token = None
+
+        while True:
+            # Préparation des paramètres de la requête
+            list_params = {
+                'Bucket': AWS_S3_BUCKET_NAME,
+                'Prefix': uuid
+            }
+            if continuation_token:
+                list_params['ContinuationToken'] = continuation_token
+
+            # Lister les objets avec les paramètres spécifiés
+            response = s3_client.list_objects_v2(**list_params)
+
+            print(response)
+            # Vérifier si des objets ont été trouvés
+            if 'Contents' in response:
+                for obj in response['Contents']:
+                    # Créer le chemin local correspondant
+                    chemin_local = os.path.join(dossier_local, os.path.relpath(obj['Key'], start=uuid))
+
+                    # Créer les dossiers locaux si nécessaire
+                    if not os.path.exists(os.path.dirname(chemin_local)):
+                        os.makedirs(os.path.dirname(chemin_local))
+
+                    # Télécharger le fichier
+                    print(f"Téléchargement de {obj['Key']} vers {chemin_local}...")
+                    s3_client.download_file(AWS_S3_BUCKET_NAME, obj['Key'], chemin_local)
+                    print("Téléchargement terminé.")
+
+            # Vérifier s'il y a plus d'objets à récupérer
+            if response.get('IsTruncated'):  # Si la liste est tronquée
+                continuation_token = response.get('NextContinuationToken')
+            else:
+                break
--- tasks.py
+++ tasks.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 
 from dotenv import load_dotenv
 
@@ -6,17 +8,16 @@ import docker
 import socketio
 from billiard.exceptions import SoftTimeLimitExceeded
 from celery import current_task
-from celery.signals import task_failure
-
+from celery.signals import task_failure, task_success
 from config import create_app
+from config import create_s3_client
+
+load_dotenv()
+SOCKETIO_SERVER_URL = os.getenv('SERVER_SOCKET_URL')
 
 flask_app = create_app()
 celery_app = flask_app.extensions["celery"]
-
-load_dotenv()
-
-# maybe change to https
-SOCKETIO_SERVER_URL = os.getenv('SERVER_SOCKET_URL')
+s3_client = create_s3_client()
 
 
 @celery_app.task(ignore_result=False, soft_time_limit=15, time_limit=20)
@@ -27,8 +28,13 @@ def run_container(image, folder_path):
     default_volume_app = "/home/executor/app/"
     absolute_folder_path = os.path.abspath(folder_path).lower()
 
+    ts = time.time()
+    print(ts)
+
     output = client.containers.run(image, remove=True, stdout=True, stderr=True,
                                    volumes=[f'{absolute_folder_path}/:{default_volume_app}'])
+
+    write_result(task_id, ts)
     publish_message(task_id, output.decode('utf-8'))
     return output.decode('utf-8')
 
@@ -46,6 +52,12 @@ def task_failure_handler(sender=run_container, **kwargs):
         publish_message(task_id, str(exception))
 
 
+@task_success.connect(sender=run_container)
+def task_failure_handler(sender=run_container, **kwargs):
+    task_id = kwargs['task_id']
+    print("je dois tout clean")
+    # clean le folder result
+
 def publish_message(task_id, result):
     clientSIO = socketio.Client()
 
@@ -59,3 +71,21 @@ def publish_message(task_id, result):
 
     clientSIO.connect(SOCKETIO_SERVER_URL)
     clientSIO.emit("task_done", {"id": task_id, "content": result})
+
+
+def write_result(path_result, ts):
+    base_folder_code_resource = os.getenv('LOCAL_STORAGE_PATH')
+    AWS_S3_BUCKET_NAME = os.getenv('AWS_S3_BUCKET_NAME')
+
+    folder_result = path_result + '/output/'
+    complete_folder_path = base_folder_code_resource + path_result + '/'
+    print(complete_folder_path)
+
+    s3_client.put_object(Bucket=AWS_S3_BUCKET_NAME, Key=folder_result)
+
+    for file in os.listdir(complete_folder_path):
+        created_time = os.path.getmtime(complete_folder_path + file)
+        if created_time > ts:
+            s3_client.upload_file(complete_folder_path + file, AWS_S3_BUCKET_NAME, folder_result + '{}'.format(file))
+
+    print("Upload results")
